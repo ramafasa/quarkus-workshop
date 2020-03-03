8e178ea3138aa094334de82256f0d903af852e55
Rafal Maciak <rafal.maciak@harman.com>
2020-03-03 22:11:34 +0100

    Fixed unit tests of Quarkus. Removed creating schema on startup. Enabled creating uberJar in Quarkus

--- vehicle-service-quarkus-app/src/test/java/com/redbend/NativeExampleResourceIT.java
+++ vehicle-service-quarkus-app/src/test/java/com/redbend/NativeExampleResourceIT.java
@@ -3,7 +3,7 @@ package com.redbend;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
+public class NativeExampleResourceIT extends VehicleResourceTest {
 
     // Execute the same tests but in native mode.
 }
