f21f818ce67d5d7d9ee2d6b384753c216c59b81f
Rafal Maciak <rafal.maciak@harman.com>
2020-03-09 09:26:18 +0100

    Added logging.

--- vehicle-service-quarkus-app/src/main/java/com/redbend/VehicleService.java
+++ vehicle-service-quarkus-app/src/main/java/com/redbend/VehicleService.java
@@ -4,6 +4,9 @@ package com.redbend;
 import com.redbend.model.Ecu;
 import com.redbend.model.Vehicle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -11,15 +14,20 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class VehicleService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(VehicleService.class);
+
     public List<Vehicle> getVehicles() {
+        LOGGER.debug("Retrieving all vehicles");
         return Vehicle.listAll();
     }
 
     public Vehicle getVehicle(Long id) {
+        LOGGER.debug("Retrieving vehicle with id {}", id);
         return Vehicle.findById(id);
     }
 
     public List<Ecu> getEcuForVehicle(Long vehicleId) {
+        LOGGER.debug("Retrieving ECUs for vehicle with id {}", vehicleId);
         return ((Vehicle) Vehicle.findById(vehicleId)).getEcuList();
     }
 }
